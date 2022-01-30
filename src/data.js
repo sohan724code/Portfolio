9dcfa8763e7a9f839cec9b8e9bf2b6ee7fea8304
sohan724code <sohanmd1375@gmail.com>
2022-01-31 00:11:01 +0600

    fix product section

--- src/data.js
+++ src/data.js
@@ -1,23 +1,27 @@
+import Recharge from "./images/screencapture-re-charge-11075-web-app-2022-01-30-23_43_24.png";
+import Eggy from "./images/screencapture-eggy-website-netlify-app-home-2022-01-30-23_49_34.png";
+import Makeup from "./images/screencapture-vigilant-shockley-e63c57-netlify-app-2022-01-30-23_46_07.png";
+import Dental from "./images/screencapture-dental-care-10-netlify-app-home-2022-01-30-23_48_19.png";
 const projects = [
   {
     id: 1,
-    img: "https://picsum.photos/seed/picsum/200/300",
-    link: "https://github.com/sohan724code",
+    img: Recharge,
+    link: "https://github.com/sohan724code/re-charge-client",
   },
   {
     id: 2,
-    img: "https://picsum.photos/seed/picsum/200/300",
-    link: "https://github.com/sohan724code",
+    img: Eggy,
+    link: "https://github.com/sohan724code/eggy-client",
   },
   {
     id: 3,
-    img: "https://picsum.photos/seed/picsum/200/300",
-    link: "https://github.com/sohan724code",
+    img: Makeup,
+    link: "https://github.com/sohan724code/good-makeup-client",
   },
   {
     id: 4,
-    img: "https://picsum.photos/seed/picsum/200/300",
-    link: "https://github.com/sohan724code",
+    img: Dental,
+    link: "https://github.com/sohan724code/health-care",
   },
 ];
 
