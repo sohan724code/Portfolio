a5bf3d039334b62cc4c0bf781f8f5a425de3c35f
sohan724code <sohanmd1375@gmail.com>
2022-02-12 06:20:34 -0800

    data.js file deleted

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,8 +1,37 @@
 import React from "react";
 import Project from "../project/Project";
 import "./Projects.css";
-import projects from "../../data";
 import { CircularProgress } from "@mui/material";
+import Recharge from "../../images/screencapture-re-charge-11075-web-app-2022-01-30-23_43_24.png";
+import Eggy from "../../images/screencapture-eggy-website-netlify-app-home-2022-01-30-23_49_34.png";
+import Makeup from "../../images/screencapture-vigilant-shockley-e63c57-netlify-app-2022-01-30-23_46_07.png";
+import Dental from "../../images/screencapture-dental-care-10-netlify-app-home-2022-01-30-23_48_19.png";
+const projects = [
+  {
+    id: 1,
+    img: Recharge,
+    link: "https://github.com/sohan724code/re-charge-client",
+    demoLink: "https://re-charge-11075.web.app/",
+  },
+  {
+    id: 2,
+    img: Eggy,
+    link: "https://github.com/sohan724code/eggy-client",
+    demoLink: "https://eggy-website.netlify.app/home",
+  },
+  {
+    id: 3,
+    img: Makeup,
+    link: "https://github.com/sohan724code/good-makeup-client",
+    demoLink: "https://vigilant-shockley-e63c57.netlify.app/",
+  },
+  {
+    id: 4,
+    img: Dental,
+    link: "https://github.com/sohan724code/health-care",
+    demoLink: "https://dental-care-10.netlify.app/home",
+  },
+];
 
 const Projects = () => {
   return (
